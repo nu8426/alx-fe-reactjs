7b9e421725044b4fee5c24fe67e9f41a4090753c
Nufis Abdena <nufisabdena@gmail.com>
2025-07-22 15:07:21 +0300

    added a route

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,11 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
-  return (
-    <div style={{ padding: '20px' }}>
+  return ( 
+    <BrowserRouter><div style={{ padding: '20px' }}>
       <h1><Link to="/">Recipe Sharing App</Link></h1>
       <Routes>
         <Route path="/" element={
@@ -17,6 +17,8 @@ function App() {
         <Route path="/recipes/:id" element={<RecipeDetails />} />
       </Routes>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
