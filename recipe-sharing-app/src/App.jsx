87b9a32c5309da47ace110ddea9830d5cad57a60
Nufis Abdena <nufisabdena@gmail.com>
2025-07-22 13:59:27 +0300

    Add recipe-sharing-app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,19 @@
+
+import AddRecipeForm from "./components/AddRecipeForm"
+import RecipeList from "./components/RecipeList"
+
+function App() {
+  
+
+  return (
+    <>
+     <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+    </>
+  )
+}
+
+export default App
