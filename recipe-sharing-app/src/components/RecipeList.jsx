6657cfc6c1549a199cff48327726bc45541249ef
Nufis Abdena <nufisabdena@gmail.com>
2025-07-25 21:11:39 +0300

    Fix: Add Link import from react-router-dom in RecipeList

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // ✅ Import Link
+import { Link } from 'react-router-dom';
 import { useRecipeStore } from '../store/recipeStore';
 
 const RecipeList = () => {
