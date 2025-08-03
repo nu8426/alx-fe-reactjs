a35550b111fc1d0709133b3b0af32f8415a7fa62
Nufis Abdena <nufisabdena@gmail.com>
2025-08-03 16:51:07 +0300

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from "./components/WelcomeMessage";
+import WelcomeMessage from "./components/Welcomemessage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
