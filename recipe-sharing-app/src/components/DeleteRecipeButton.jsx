d68f8a16bef2f2f574d746272ed7482acd940b36
Nufis Abdena <nufisabdena@gmail.com>
2025-08-03 15:46:36 +0300

    installed zustand

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
