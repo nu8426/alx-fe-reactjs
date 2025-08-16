e28daf16cf2f9ca49dfd5abbe2381c9ed7655779
Nufis Abdena <nufisabdena@gmail.com>
2025-08-16 13:40:55 +0300

    Update App.jsx
    update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
+
 function App() {
   return (
-    <h1 className="text-blue-500 text-2xl">Recipe Sharing Platform</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
