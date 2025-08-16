0b023ac8a357102d13391542e1582dbfa443f3e6
Nufis Abdena <nufisabdena@gmail.com>
2025-08-16 17:45:02 +0300

    Update UserProfile.jsx
    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,15 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition">
-      <img 
-        src="https://via.placeholder.com/150" 
-        alt="User" 
-        className="rounded-full w-24 sm:w-36 h-24 sm:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" 
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-2 sm:my-4 text-center hover:text-blue-500 transition-colors">
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-2 md:my-4 text-center hover:text-blue-500 transition-colors">
         John Doe
       </h1>
-      <p className="text-gray-600 text-sm sm:text-base text-center">
+      <p className="text-gray-600 sm:text-sm md:text-base text-center">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
