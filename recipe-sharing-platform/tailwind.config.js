48237a49be276800d9238da183ecb716189cb2d2
Nufis Abdena <nufisabdena@gmail.com>
2025-08-16 13:18:30 +0300

    Update tailwind.config.js
    added

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
     extend: {},
