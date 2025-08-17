f7d32cf4321861ffd18cd62896caeb981a277982
Nufis Abdena <nufisabdena@gmail.com>
2025-08-17 11:20:17 +0300

    Create  App.jsx
    Create  App.jsx

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,20 @@
+// src/App.jsx
+import React from 'react';
+import UserContext from './UserContext';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  const userData = {
+    name: 'Alice',
+    age: 25,
+    bio: 'Loves hiking and photography',
+  };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <UserProfile />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
