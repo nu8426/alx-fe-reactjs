789829487d90956dd7e56f5f95d107110c8ed33f
Nufis Abdena <nufisabdena@gmail.com>
2025-08-17 11:23:37 +0300

    Create UserProfile.jsx
    Create UserProfile.jsx

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import UserContext from '../UserContext';
+
+const UserProfile = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p>Age: {user.age}</p>
+      <p>Bio: {user.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
