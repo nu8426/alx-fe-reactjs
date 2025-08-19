11505b32b9192dade16b13fa0f270ff39e941deb
Nufis Abdena <nufisabdena@gmail.com>
2025-08-19 12:08:02 +0300

    Create App.jsx
    add App.jsx

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TodoList from "./components/TodoList.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <h1>React Todo App</h1>
+      <TodoList />
+    </div>
+  );
+}
