2928d2d14046bf55f5f9baed7e47f2b5f1a4f926
Nufis Abdena <nufisabdena@gmail.com>
2025-08-19 11:09:37 +0300

    Update main.jsx
    update main.jsx

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
-import PostsComponent from "./components/PostsComponent.jsx";
-
-const queryClient = new QueryClient();
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
