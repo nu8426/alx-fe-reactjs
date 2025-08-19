569c2f86fb36dcc0e2a30f6e30df34127cf0dd95
Nufis Abdena <nufisabdena@gmail.com>
2025-08-19 11:47:52 +0300

    Update Profile.jsx
    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,13 +1,14 @@
-
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useParams } from "react-router-dom";
 import ProfileDetails from "./ProfileDetails.jsx";
 import ProfileSettings from "./ProfileSettings.jsx";
 
 export default function Profile() {
+  const { id } = useParams(); // dynamic route param
+
   return (
     <div>
-      <h2>Profile Page</h2>
+      <h2>Profile Page - User ID: {id}</h2>
       <nav>
         <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
       </nav>
