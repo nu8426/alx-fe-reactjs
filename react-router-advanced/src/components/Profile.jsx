52f3b7167cde1040276491cc3a28169f33260c63
Nufis Abdena <nufisabdena@gmail.com>
2025-08-19 11:40:08 +0300

    Create Profile.jsx
    add Profile.jsx

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails.jsx";
+import ProfileSettings from "./ProfileSettings.jsx";
+
+export default function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <nav>
+        <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
+      </nav>
+
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+        <Route path="" element={<p>Select a sub-section.</p>} />
+      </Routes>
+    </div>
+  );
+}
