f5ae9c6371b99ea5bb4c84de3aae3b5eff33c8f8
Nufis Abdena <nufisabdena@gmail.com>
2025-08-19 10:11:20 +0300

    Create MainContent.jsx
    add MainContent.jsx

---
+++ alx-react-app-new/src/ components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '15px', fontSize: '18px', color: '#333' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+export default MainContent;
