8b1c263899584c4cc81d8d55f85b6cc4eb83c104
Nufis Abdena <nufisabdena@gmail.com>
2025-08-19 12:01:25 +0300

    Update App.jsx
    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,10 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import useAuth from "./hooks/useAuth.jsx";
+import Home from "./components/Home.jsx";
+import Profile from "./components/Profile.jsx";
+import BlogPost from "./components/BlogPost.jsx";
+import Login from "./components/Login.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 export default function App() {
   const { isAuthenticated, login, logout } = useAuth();
 
   return (
-    <Router>
+    <BrowserRouter>
       <nav>
         <Link to="/">Home</Link> |{" "}
         <Link to="/profile/123">Profile</Link> |{" "}
@@ -20,11 +27,26 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login login={login} />} />
 
-        <Route path="/profile/:id/*" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-        <Route path="/blog/:id" element={<ProtectedRoute><BlogPost /></ProtectedRoute>} />
+        {/* Protected Routes */}
+        <Route
+          path="/profile/:id/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/blog/:id"
+          element={
+            <ProtectedRoute>
+              <BlogPost />
+            </ProtectedRoute>
+          }
+        />
 
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
