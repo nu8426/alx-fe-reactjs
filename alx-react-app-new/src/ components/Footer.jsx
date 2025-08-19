ada597ec74c11254d43eb1a016ce67b52313fe99
Nufis Abdena <nufisabdena@gmail.com>
2025-08-19 10:09:48 +0300

    Create Footer.jsx
    add Footer.jsx

---
+++ alx-react-app-new/src/ components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#f2f2f2', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+export default Footer;
