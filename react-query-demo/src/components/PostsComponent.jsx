79497fe702e36d955235c5529b84b2be4c10ded2
Nufis Abdena <nufisabdena@gmail.com>
2025-08-19 11:27:51 +0300

    Update PostsComponent.jsx
    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 
-// function to fetch posts
+// Function to fetch posts
 const fetchPosts = async () => {
   const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
   return res.data;
 };
 
 export default function PostsComponent() {
-  const { data, isLoading, isError, error, refetch } = useQuery("posts", fetchPosts);
+  const { data, isLoading, isError, error, refetch } = useQuery("posts", fetchPosts, {
+    cacheTime: 1000 * 60 * 5,          // Cache data for 5 minutes
+    staleTime: 1000 * 60,              // Data considered fresh for 1 minute
+    refetchOnWindowFocus: false,       // Do not refetch when window refocuses
+    keepPreviousData: true,            // Keep previous data while fetching new
+  });
 
   if (isLoading) return <p>Loading posts...</p>;
   if (isError) return <p>Error loading posts: {error.message}</p>;
